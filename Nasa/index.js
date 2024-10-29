dfbaeedcb007c22b1224f7759a9d2f6a683d3949
christian-racsic <christian_canals@outlook.es>
2024-10-29 23:59:40 +0100

    final practica api

--- Nasa/index.js
+++ Nasa/index.js
@@ -18,25 +18,35 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
 
     async function getAndRenderImgOfTheDay() {
+        container.innerHTML = "";
         const result = await fetchAPI();
 
         let divCharacter = document.createElement("div");
         divCharacter.classList.add("col");
         divCharacter.innerHTML = `
-            <div class="card" style="width: 18rem;">
-                <img src="${result.url}" class="card-img-top" alt="NASA Image">
+            <div class="card" style="width: 70rem;">
+                <img src="${result.hdurl}" class="card-img-top" alt="NASA Image">
                 <div class="card-body">
                     <h5 class="card-title">${result.title}</h5>
-                    <p class="card-text">${result.explanation}</p>
-                    <button href="#" class="btn btn-primary">Details</button>
+                    <button href="#" id="details" class="btn btn-primary">Details</button>
                 </div>
             </div>
-            <h2>Selecciona una fecha</h2>
-            <input type="date" id="datePicker">
-            <p id="selectedDate"></p>
+            <div class="container mt-4">
+    <h2 class="mb-3">Selecciona una fecha para ver la foto </h2>
+    <div class="form-group">
+        <input type="date" id="datePicker" class="form-control mb-3" style="max-width: 300px;">
+        <p id="selectedDate" class="text-muted"></p>
+    </div>
+</div>
         `;
 
         container.appendChild(divCharacter);
+
+        divCharacter.querySelector("#details").addEventListener("click", () => {
+            
+            detailsDay(result);
+        });
+
 
         const datePicker = divCharacter.querySelector("#datePicker");
         datePicker.addEventListener("change", () => {
@@ -45,13 +55,46 @@ document.addEventListener("DOMContentLoaded", async () => {
             getAndRenderSelectedImg(formattedDate);
         });
     }
-    
+
+    function detailsDay(result){
+        container.innerHTML = "";
+        let detailsDiv = document.createElement("div");
+        detailsDiv.classList.add("col");
+
+        detailsDiv.innerHTML = `
+            <div class="card" style="width: 70rem;">
+                <img src="${result.hdurl}" class="card-img-top" alt="NASA Image">
+                <div class="card-body">
+                    <h5 class="card-title">${result.title}</h5>
+                    <p class="card-text">${result.date}</p>
+                    <p class="card-text">${result.explanation}</p>
+                    <button href="#" id="principal" class="btn btn-primary">Details</button>
+                </div>
+            </div>
+            <div class="container mt-4">
+    <h2 class="mb-3">Selecciona una fecha para ver la foto </h2>
+    <div class="form-group">
+        <input type="date" id="datePicker" class="form-control mb-3" style="max-width: 300px;">
+        <p id="selectedDate" class="text-muted"></p>
+    </div>
+</div>
+        `;
+
+        container.appendChild(detailsDiv);
+
+        detailsDiv.querySelector("#principal").addEventListener("click", () => {
+            
+            getAndRenderImgOfTheDay();
+        });
+        
+    }
+
     async function getAndRenderSelectedImg(formattedDate) {
         const result = await fetchAPISelec(formattedDate);
 
         container.innerHTML = `
-            <div class="card" style="width: 18rem;">
-                <img src="${result.url}" class="card-img-top" alt="NASA Image">
+            <div class="card" style="width: 70rem;">
+                <img src="${result.hdurl}" class="card-img-top" alt="NASA Image">
                 <div class="card-body">
                     <h5 class="card-title">${result.title}</h5>
                     <p class="card-text">${result.explanation}</p>
